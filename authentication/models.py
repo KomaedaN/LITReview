1d99c82b2a860bc5dc51ce3f1ca3f829b7268b22
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-10-30 16:49:37 +0100

    Create all models authentication and review

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    profil_pic = models.ImageField(verbose_name='photo de profil', blank=True)
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+
+    class Meta:
+        unique_together = ('user', 'followed_user',)
