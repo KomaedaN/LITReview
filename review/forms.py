0710934e5b04d9d775062f481ebb5da9bf75c6a2
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-11-09 00:03:39 +0100

    Update review html

--- review/forms.py
+++ review/forms.py
@@ -3,7 +3,7 @@ from . import models
 
 
 class TicketForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, label='Title',
+    title = forms.CharField(max_length=128, label='Titre',
                             widget=forms.TextInput(attrs={'class': "create_ticket_forms create_ticket_forms_title"}))
 
     description = forms.CharField(max_length=2048, label='Description',
@@ -18,12 +18,12 @@ class ReviewForm(forms.ModelForm):
     RATING_CHOICE = [
         ("0", 0), ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5),
     ]
-    headline = forms.CharField(max_length=128, label='Title',
+    headline = forms.CharField(max_length=128, label='Note',
                             widget=forms.TextInput(attrs={'class': "create_ticket_forms create_ticket_forms_title"}))
 
-    rating = forms.ChoiceField(widget=forms.RadioSelect, choices=RATING_CHOICE,)
+    rating = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class': "ul_review"}), choices=RATING_CHOICE,)
 
-    body = forms.CharField(max_length=2048, label='Description',
+    body = forms.CharField(max_length=2048, label='Commentaire',
                            widget=forms.Textarea(attrs={'class': "create_ticket_forms"}))
 
     class Meta:
